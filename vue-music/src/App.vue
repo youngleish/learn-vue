38a3798243edd98936f9bc7a0e16adba08773c58
youngleish <youngleish@gmail.com>
2017-07-16 18:05:28 +0800

    finish component header

--- vue-music/src/App.vue
+++ vue-music/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-        <header></header>
+        <Header></Header>
         <router-view></router-view>
     </div>
 </template>
--- vue-music/src/components/header/header.vue
+++ vue-music/src/components/header/header.vue
@@ -11,11 +11,36 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss" type="stylesheet/scss">
-    @import "../../common/sass/variable.scss";
-    header{
+<style lang="scss" rel="stylesheet/scss">
+    @import "../../common/sass/variable";
+    @import "../../common/sass/mixin";
+    .header {
         position: relative;
-        height:44px;
-        background-color:$color-theme;
+        height: 44px;
+        line-height:44px;
+        color: $color-theme;
+        text-align: center;
+        &-logo{
+            display: inline-block;
+            vertical-align: middle;
+            width:30px;
+            height:32px;
+            margin-right:9px;
+            @include bg-image('./logo');
+            background-repeat:no-repeat;
+            background-size:100% ;
+        }
+        &-title {
+            display: inline-block;
+            font-size: 18px;
+        }
+        .icon-mine{
+            position: absolute;
+            right:0;
+            top:50%;
+            transform: translateY(-50%);
+            padding-right:12px;
+            font-size:20px;
+        }
     }
 </style>
