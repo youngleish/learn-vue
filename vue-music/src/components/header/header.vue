430c481b37e5162d7264752113fabe42121ae2ed
youngleish <youngleish@gmail.com>
2017-07-16 17:19:55 +0800

    修改eslint和webpack配置

---
+++ vue-music/src/components/header/header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="header">
+        <div class="header-logo"></div>
+        <h1 class="header-title">music</h1>
+        <i class="icon icon-mine"></i>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss" type="stylesheet/scss">
+    @import "../../common/sass/variable.scss";
+    header{
+        position: relative;
+        height:44px;
+        background-color:$color-theme;
+    }
+</style>
--- vue-music/src/main.js
+++ vue-music/src/main.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import 'common/sass/index.scss'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+    el: '#app',
+    router,
+    render: h => h(App)
 })
