27b4526787f200d7b92a64ea9c6372d20f5cb51e
youngleish <youngleish@gmail.com>
2017-11-23 22:00:39 +0800

    热门歌单列表添加图片懒加载

--- vue-music/src/main.js
+++ vue-music/src/main.js
@@ -3,10 +3,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
 
 import 'common/sass/index.scss'
 Vue.config.productionTip = false
 Fastclick.attach(document.body)
+
+Vue.use(VueLazyload, {
+    loading: require('common/images/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({
